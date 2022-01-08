5f61010e5244732c203a29c67e1c370a656746ab
DiSe <13914438563@163.com>
2022-01-08 19:02:45 +0800

    master

--- src/components/Success.vue
+++ src/components/Success.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="success">
     <icon name="check-circle" style="color: #00a870" size="72px"/>
+    <span>项目已创建成功</span>
     <div class="btnCtn">
       <t-button theme="primary">返回首页</t-button>
-      <t-button theme="default">填充按钮</t-button>
+      <t-button theme="default">查看详情</t-button>
     </div>
   </div>
 </template>
 
 <script>
 import { Icon } from 'tdesign-icons-vue';
-
 
 export default {
   name: 'Success',
@@ -21,10 +21,32 @@ export default {
   },
   components: {
     Icon
+  },
+  methods: {
+    
   }
 }
 </script>
 
 <style scoped>
+.success {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 75vh;
+}
 
+span {
+  display: block;
+  font-size: 20px;
+  line-height: 28px;
+  font-weight: 500;
+  margin-top: 28px;
+  margin-bottom: 32px;
+}
+
+button {
+  margin: 0 4px;
+}
 </style>
