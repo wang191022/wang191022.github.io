e41db6829e74c348dac27034392ca98c5b0e2f34
DiSe <13914438563@163.com>
2022-01-08 16:44:34 +0800

    master

---
+++ src/components/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="notFound">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Success.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="success">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Success',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
