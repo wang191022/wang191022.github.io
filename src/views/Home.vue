2901a90c34b69736bb4da4d3dfdd66f14350b259
DiSe <13914438563@163.com>
2022-01-08 19:20:43 +0800

    master

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="home">
-    <Success/>
+    
   </div>
 </template>
 
 <script>
-import Success from '@/components/Success'
-
 export default {
   name: 'Home',
   components: {
-    Success
   }
 }
 </script>
