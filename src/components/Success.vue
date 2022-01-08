656924dd287e191b96f4d535a296a0fcfe2d4be0
DiSe <13914438563@163.com>
2022-01-08 18:42:40 +0800

    master

--- src/components/Success.vue
+++ src/components/Success.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="success">
-    
+    <icon name="check-circle" style="color: #00a870" size="72px"/>
+    <div class="btnCtn">
+      <t-button theme="primary">返回首页</t-button>
+      <t-button theme="default">填充按钮</t-button>
+    </div>
   </div>
 </template>
 
 <script>
+import { Icon } from 'tdesign-icons-vue';
+
+
 export default {
   name: 'Success',
   data() {
     return {
       
     }
+  },
+  components: {
+    Icon
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
-
+    <Success/>
   </div>
 </template>
 
 <script>
+import Success from '@/components/Success'
+
 export default {
   name: 'Home',
   components: {
+    Success
   }
 }
 </script>
