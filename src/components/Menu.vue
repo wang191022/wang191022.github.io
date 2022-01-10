b62c23f049652d3c5560eefe97dee241028facf0
DiSe <13914438563@163.com>
2022-01-10 13:12:37 +0800

    master

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="menu">
-     <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo">
-      <router-link v-for="(item, index) in menu" :key="index" :to="item.route">
-        <el-menu-item :key="item.index">
-          <i :class="item.icon"></i>
-          <span slot="title">{{item.title}}</span>
-        </el-menu-item>
-      </router-link>
-     </el-menu>
+    <el-menu default-active="-1" class="el-menu-vertical-demo">
+        <router-link
+          v-for="(item, index) in menu"
+          :key="index"
+          :to="item.route">
+          <el-menu-item
+            :index="index + ''">
+            <i :class="item.icon"></i>
+            <span> {{ item.title }} </span>
+          </el-menu-item>
+        </router-link>
+    </el-menu>
   </div>
 </template>
 
@@ -20,38 +22,36 @@ export default {
     return {
       menu: [
         {
-          name: "list",
-          icon: "bulletpoint",
+          icon: "el-icon-document",
           title: "列表总览",
           route: { name: "List" },
         },
         {
-          name: "detail",
-          icon: "calendar",
+          icon: "el-icon-notebook-2",
           title: "表单详情",
           route: { name: "Detail" },
         },
         {
-          name: "form",
-          icon: "root-list",
+          icon: "el-icon-edit",
           title: "表单编辑",
           route: { name: "Form" },
         },
         {
-          name: "login",
-          icon: "login",
+          icon: "el-icon-setting",
           title: "退出登录",
           route: { name: "Login" },
         },
       ],
     };
   },
+  methods: {
+  },
 };
 </script>
 
 <style scoped>
-.menu {
-  height: 100vh;
+.el-menu-vertical-demo {
+  height: 98vh;
 }
 
 a {
